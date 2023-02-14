fe36f269014fb560d9d42d58d626d799d0af454b
Nimet Ozkan <nimozkan@Nimets-MacBook-Pro.local>
2023-02-13 20:17:48 -0600

    added & tested forms.py

--- website/views.py
+++ website/views.py
@@ -1,7 +1,17 @@
 from flask import Blueprint ,render_template
+from .forms import *
 
 views = Blueprint("views",__name__)
 
 @views.route("/")
 def home():
     return render_template("home.html")
+
+
+@views.route("/signup", methods=['GET', 'POST'])
+def signup():
+    form = UserForm()
+    if form.validate_on_submit():
+        # Save the user information to database or perform other actions
+        return redirect(url_for('home'))
+    return render_template('signup.html', form=form)
