c638e77e534d9c040aec601adb4648f347b89260
Nimet Ozkan <nimozkan@Nimets-MacBook-Pro.local>
2023-02-27 02:08:09 -0600

    tried & added creating database models

--- website/models.py
+++ website/models.py
@@ -1,4 +1,29 @@
-from .import db
-from flask_login import UserMixin
-from sqlalchemy.sql import func
+from datetime import datetime
+from sqlalchemy.orm import relationship
+from .config import db
+
+class UserCredentials(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+    client_id = db.Column(db.Integer, db.ForeignKey('client_information.id'))
+
+class ClientInformation(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    full_name = db.Column(db.String(50), nullable=False)
+    address1 = db.Column(db.String(100), nullable=False)
+    address2 = db.Column(db.String(100))
+    city = db.Column(db.String(100), nullable=False)
+    state = db.Column(db.String(2), nullable=False)
+    zipcode = db.Column(db.String(9), nullable=False)
+    user_credentials = relationship("UserCredentials", backref="client_information")
+
+class FuelQuote(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    gallons_requested = db.Column(db.Float, nullable=False)
+    delivery_address = db.Column(db.String(200), nullable=False)
+    delivery_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    suggested_price = db.Column(db.Float)
+    total_amount_due = db.Column(db.Float)
+    client_id = db.Column(db.Integer, db.ForeignKey('client_information.id'))
 
