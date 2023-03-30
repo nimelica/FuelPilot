30d0492c193817007fd68570b5dc830467243182
Nimet Ozkan <nimozkan@Nimets-MacBook-Pro.local>
2023-03-29 19:32:53 -0500

    new branch to test backend

--- website/views.py
+++ website/views.py
@@ -1,6 +1,7 @@
-from flask import Blueprint ,render_template,redirect,url_for,flash
+from flask import Blueprint ,render_template, session, flash, redirect, url_for
 from .forms import *
 from .models import *
+from werkzeug.security import generate_password_hash, check_password_hash
 
 views = Blueprint("views",__name__)
 
@@ -10,11 +11,37 @@ def home():
     title = 'Home'
     return render_template("home.html",form=form,title=title)
 
+@views.route("/signup", methods=['GET', 'POST'])
+def signup():
+    form = UserForm()
+    title = 'Register'
+    if form.validate_on_submit():
+        # Check if the username already exists in the database
+        existing_user = UserCredentials.query.filter_by(username=form.username.data).first()
+        if existing_user:
+            flash('Username already exists')
+            return redirect(url_for('views.signup'))
+
+        # Create a new UserCredentials object and add it to the database
+        new_user = UserCredentials(username=form.username.data, password=form.password.data)
+        db.session.add(new_user)
+        db.session.commit()
+
+        # Set the user's ID as a session variable so they are logged in
+        session['user_id'] = new_user.id
+
+        flash('Account created successfully')
+        return redirect(url_for('views.home'))
+
+    return render_template('signup.html', form=form, title=title)
+
+
 @views.route("/fuel_quote_form", methods=['GET', 'POST'])
 def fuel_quote_form():
     form = QuoteFuelForm()
     title = 'Quote Form'
     return render_template('fuel_quote_form.html', form=form, title=title)
+
 
 @views.route("/fuel_quote_history", methods=['GET', 'POST'])
 def fuel_quote_history():
@@ -22,34 +49,36 @@ def fuel_quote_history():
     title = 'Quote History'
     return render_template('fuel_quote_history.html', form=form, title=title)
 
-@views.route("/signup", methods=['GET', 'POST'])
-def signup():
-    form = UserForm()
-    title = 'Register'
-    if form.validate_on_submit():
-        flash("Successfully created Account!",category="success")
-        return  redirect(url_for('views.home'))
-    return render_template('signup.html', form=form, title=title)
 
-@views.route("/login",methods=["GET","POST"])
+# TODO: add user-profile dashboard page
+@views.route("/login", methods=["GET", "POST"])
 def login():
-   form = LoginForm()
-   title = 'Login'
-   if form.validate_on_submit():
-        #check whether username/password if they exist rout to home page else alert user invalid input and rerender the template
-        if form.username.data == "user1" and form.password.data == "1234":
-            flash("Login was Successful!",category="success")
-            return  redirect(url_for('views.home'))
-        else:
-            flash("Invalid Username or Password please try again.",category="error")
-            return render_template('login.html', form=form, title=title)
-   return render_template('login.html', form=form, title=title)
+    form = LoginForm()
+    title = 'Login'
+    # check submission and if login is in database
+    if form.validate_on_submit():
+        # Get the username and password from the form
+        username = form.username.data
+        password = form.password.data
+        
+        # Check if the username exists in the database
+        user = UserCredentials.query.filter_by(username=username).first()
+
+        # If the username exists, check if the password is correct
+        if user and user.check_password(password):
+            # If the password is correct, log the user in and redirect them to the home page
+            session['user_id'] = user.id
+            return redirect(url_for('views.home'))
+        
+        # If the username or password is incorrect, show an error message
+        flash('Invalid username or password', 'error')
+        
+    # Render the login page with the login form
+    return render_template('login.html', form=form, title=title)
+
 
 @views.route("/profile_management", methods=['GET', 'POST'])
 def profile_management():
     form = InfoForm()
     title = 'Profile'
-    if form.validate_on_submit():
-        flash("Successfully registered Profile!",category="success")
-        return  redirect(url_for('views.home'))
     return render_template('profile_management.html', form=form, title=title)
