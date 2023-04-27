f05d36d9a78bac2fa4e1f7e6a3dcbfa761c7cda1
henrychen71 <henrychen71@gmail.com>
2023-04-27 12:11:44 -0500

    fixed

--- website/views.py
+++ website/views.py
@@ -146,6 +146,12 @@ def login():
 def profile_management():
     form = InfoForm()
     title = 'Profile'
+    if 'user_id' not in session:
+        flash('You must sign in before accessing Profile.')
+        return redirect(url_for('views.login'))
+    
+    client_info = ClientInformation.query.filter_by(user_credentials_id=session['user_id']).first()
+    client_information = ClientInformation.query.filter_by(user_credentials_id=session['user_id']).all()
     # if form.validate_on_submit():
     if request.method == 'GET' and len(client_information) != 0:
         flash('You have already made a profile. However, you may edit at any time.')
